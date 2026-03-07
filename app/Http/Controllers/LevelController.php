49facfdebf1d305b89d7e4ed951b7acc98f8f17c
Tsabita Keizya Vanya Putri <tsabitakeizya@gmail.com>
2026-03-07 15:22:29 +0700

    PWL_Jb3_Prak4

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Http\Request;
+
+class LevelController extends Controller
+{
+    public function index()
+    {
+        // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', 
+        // ['CUS', 'Pelanggan', now()]);
+
+        // return 'Insert data baru berhasil';
+
+        // $row =  DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // return 'Update data berhasil. Jumlah data yang di update: ' .$row.' baris';
+
+        // $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        // return 'Data level berhasil dihapus. Jumlah data yang dihapus: ' .$row.' baris';
+
+        $data = DB::select('select * from m_level');
+        return view('level', ['data' => $data]);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/level', [LevelController::class, 'index']);
 
     Route::prefix('category')->group(function () {
     Route::get('/food-beverage', function () {
