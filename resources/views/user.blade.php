1cb5f5668dfb2a5adff6bffbf81f133e8bd380f8
Tsabita Keizya Vanya Putri <tsabitakeizya@gmail.com>
2026-03-11 15:03:35 +0700

    PWL_Jb4_Prak1

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,16 +1,32 @@
+<!DOCTYPE html>
 <html>
 <head>
-    <title>User</title>
+    <title>Data User</title>
 </head>
 <body>
 
-    <h1>Data User</h1>
+<h1>Data User</h1>
 
-    <p>ID User: {{ $id }}</p>
-    <p>Nama User: {{ $name }}</p>
+<table border="1" cellpadding="2" cellspacing="0">
+    <tr>
+        <th>ID</th>
+        <th>Level ID</th>
+        <th>Username</th>
+        <th>Nama</th>
+        <th>Password</th>
+    </tr>
 
-    <br>
-    <a href="/POS/public">Kembali ke Home</a>
+    @foreach ($data as $d)
+    <tr>
+        <td>{{ $d->user_id }}</td>
+        <td>{{ $d->level_id }}</td>
+        <td>{{ $d->username }}</td>
+        <td>{{ $d->nama }}</td>
+        <td>{{ $d->password }}</td>
+    </tr>
+    @endforeach
+
+</table>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,17 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
+
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
-
+Route::get('/user', [UserController::class, 'index']);
     Route::prefix('category')->group(function () {
     Route::get('/food-beverage', function () {
         return view('category.food');
