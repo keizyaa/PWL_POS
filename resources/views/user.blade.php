95441b7d9dcbc8e57d3be37d4cb217994569132e
Tsabita Keizya Vanya Putri <tsabitakeizya@gmail.com>
2026-03-11 15:39:39 +0700

    PWL_Jb4_Prak2.1

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,19 +10,17 @@
 <table border="1" cellpadding="2" cellspacing="0">
     <tr>
         <th>ID</th>
-        <th>Level ID</th>
         <th>Username</th>
         <th>Nama</th>
-        <th>Password</th>
+        <th>ID Level Pengguna</th>
     </tr>
 
     @foreach ($data as $d)
     <tr>
-        <td>{{ $d->user_id }}</td>
-        <td>{{ $d->level_id }}</td>
-        <td>{{ $d->username }}</td>
-        <td>{{ $d->nama }}</td>
-        <td>{{ $d->password }}</td>
+        <td>{{ $data->user_id }}</td>
+        <td>{{ $data->username }}</td>
+        <td>{{ $data->nama }}</td>
+        <td>{{ $data->level_id }}</td>
     </tr>
     @endforeach
 
